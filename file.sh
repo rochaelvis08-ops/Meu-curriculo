fc0254b1e6e0e1a5a989b649d36ef842bb3d650d
rochaelvis08-ops <rochaelvis08@gmail.com>
2026-03-14 18:59:13 -0300

    Add files via upload

---
+++ file.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Primeira versão do site profissional"
+git branch -M main
+git remote add origin https://github.com/SEU-USUARIO/elvis-site.git
+git push -u origin main
